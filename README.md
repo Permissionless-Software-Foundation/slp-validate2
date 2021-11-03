fdf28a058362e85db2880e2f9a8b5b5e77c3c8d2
Chris Troutner <chris.troutner@gmail.com>
2021-11-03 12:28:10 -0700

    got first integration test for crawlDag()

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 This is a JavaScript npm library for validating SLP token transactions. It's an alternative implementation of the [slp-validate](https://github.com/simpleledger/slp-validate.js). It's tested against the same test data.
 
+The primary focus
+of this library is to validate an SLP TXID. At a high-level, validation follows
+this workflow:
+
+- Get TX data from a full node (or DB cache)
+- Traverse the DAG to the GENESIS transaction
+- Ensure each TX in the DAG has:
+  - a valid SLP OP_RETURN
+  - Token ID matches for all TXs
+- Check for corner cases
+
 ## Install
 
 Setup development environment and run tests:
--- index.js
+++ index.js
@@ -1,28 +1,47 @@
 /*
-  An npm JavaScript library for front end web apps. Implements a minimal
-  Bitcoin Cash wallet.
-
-  test
+  A blank-canvas reimplentation of the slp-validate library. The primary focus
+  of this library is to validate an SLP TXID. At a high-level, validation follows
+  this workflow:
+  - Get TX data from a full node (or DB cache)
+  - Traverse the DAG to the GENESIS transaction
+  - Ensure each TX in the DAG has:
+    - a valid SLP OP_RETURN
+    - Token ID matches for all TXs
+  - Check for corner cases
 */
 
 /* eslint-disable no-async-promise-executor */
 
 'use strict'
 
-const BCHJS = require('@psf/bch-js')
+const Cache = require('./lib/cache')
+const DAG = require('./lib/dag')
 
-const Util = require('./lib/util')
-const util = new Util()
+class SlpValidate {
+  constructor (localConfig = {}) {
+    // Dependency injection
+    this.bchjs = localConfig.bchjs
+    if (!this.bchjs) {
+      throw new Error(
+        'Must inject instance of bch-js when instantiating SlpValidate'
+      )
+    }
 
-let _this // local global for 'this'.
+    // Encapsulate dependencies
+    this.cache = new Cache(localConfig)
+    localConfig.cache = this.cache
+    this.dag = new DAG(localConfig)
+  }
 
-class BoilplateLib {
-  constructor () {
-    _this = this
-
-    _this.bchjs = new BCHJS()
-    _this.util = util
+  // Given a txid, recursivly crawl the DAG and validate the SLP transaction.
+  async validateTxid (txid) {
+    try {
+      console.log('hello world')
+    } catch (err) {
+      console.error('Error in validateTxid()')
+      throw err
+    }
   }
 }
 
-module.exports = BoilplateLib
+module.exports = SlpValidate
